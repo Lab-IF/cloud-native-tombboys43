e2e0450cca30f4bf6c5d1dd9222416351554a981
TOMMY KURNIAWAN <150642593+tombboys43@users.noreply.github.com>
2026-04-29 06:22:01 +0000

    tugas pertemuan 1

---
+++ kantor-ex/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>saya bung tomy</h1>
+      <p>jangan macam macam sama saya karena saya murid gufron</p>
+      <p>passobis handal sekali</p>
+      
+    </main>
+  );
+}
---
+++ pertemuan-00/about/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>saya tomy</h1>
+      <p>Ini proyek Next.js pertama saya.</p>
+      <p>Menggunakan <strong>JavaScript</strong>, bukan TypeScript.</p>
+    </main>
+  );
+}
